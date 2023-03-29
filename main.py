66854c958f3fc16271e1df0fdab51124f9928d36
francisco melazzini <francisco.melazzini@physics.msu.ru>
2023-03-29 16:38:15 +0300

    added build_log_empty_spectrum_count to build empty agn spectra

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 from spectrum_utils import *
 from colum_density_utils import *
 
+empty_spectrum = PoissonSpectrumCountFactory.build_log_empty_spectrumcount(100,300_000,2000)
 
-index = get_interval_index_log(value=1.75e24, value_interval=ColumnDensityInterval(
-    LEFT_NH, RIGHT_NH), num_of_intervals=NH_INTERVALS)
-# print(index)
+empty_spectrum[1997].y=777
+empty_spectrum[1997].y_err=12
 
-d_nh_expo = (np.log10(RIGHT_NH)-np.log10(LEFT_NH))/NH_INTERVALS
+for item in empty_spectrum:
+    print(item)
 
-nh_left = LEFT_NH
-
-nh_list = []
-
-for i in range(NH_INTERVALS):
-    nh_ip1 = LEFT_NH*10**(d_nh_expo*(i+1))
-    nh_i = LEFT_NH*10**(d_nh_expo*i)
-    d_nh = nh_ip1 - nh_i
-    nh_list += [nh_i + (d_nh/2)]
-
-
-# print(nh_list)
-# print(len(nh_list))
-
-nh_bounds = np.logspace(np.log10(LEFT_NH), np.log10(RIGHT_NH), NH_INTERVALS+1)
-
-for i, nh_left_val in enumerate(nh_bounds[:-1]):
-    nh_i = (nh_left_val+nh_bounds[i+1])/2
-    # print(f'#{i+1:>2} N_H={nh_i:0.3e}cm-2')
-    interval = ColumnDensityInterval(left=nh_left_val, right=nh_bounds[i+1])
-    # print(interval)
-
-
-nh_grid = ColumnDensityGrid(left_nh=1e22, right_nh=2e24, n_intervals=30)
-
-print(nh_grid.index(nh=1e23))
-
+print(empty_spectrum[-1])
--- spectrum_utils.py
+++ spectrum_utils.py
@@ -33,6 +33,8 @@ class SpectrumBase:
         y: the spectrum values, for example flux-density
 
         y_err: the error on y values, for example the standard deviation
+        
+        Both x,y,y_err are grouped into @SpectrumBaseItem objects
 
     The user has to determine the meaning and units of x and y.
 
@@ -50,11 +52,13 @@ class SpectrumBase:
             y_err (Iterable[float]): the error on y values, for example the standard deviation
         """
         if len(x) == len(y) == len(y_err):
-            self.x = x
-            self.y = y
-            self.y_err = y_err
             self.length = len(x)
             self.pos = 0
+
+            self.spectrum_list = np.empty(len(x), dtype=SpectrumBaseItem)
+            for i in range(len(x)):
+                self.spectrum_list[i] = SpectrumBaseItem(x[i], y[i], y_err[i])
+
         else:
             raise ValueAndError('The length of x,y, and y_err must be equal!')
 
@@ -62,7 +66,7 @@ class SpectrumBase:
         pos = self.pos
         if pos < self.length:
             self.pos += 1
-            return self.x[pos], self.y[pos], self.y_err[pos]
+            return self.spectrum_list[pos]
         else:
             self.pos = 0
             raise StopIteration
@@ -71,118 +75,118 @@ class SpectrumBase:
         return self
 
     def __getitem__(self, index: int) -> SpectrumBaseItem:
-        return SpectrumBaseItem(x=self.x[index], y=self.y[index], y_err=self.y_err[index])
+        
+        return self.spectrum_list[index]
 
     def __len__(self) -> int:
         return len(self.x)
 
-    def components(self) -> Tuple[Iterable[float], Iterable[float], Iterable[float]]:
-        """Returns a tuple with the copy of
-        the x, y and y_err components
-        of the spectrum
+    # def components(self) -> Tuple[Iterable[float], Iterable[float], Iterable[float]]:
+    #     """Returns a tuple with the copy of
+    #     the x, y and y_err components
+    #     of the spectrum
 
-        Returns:
-            Tuple[Iterable[float], Iterable[float], Iterable[float]]: x,y,y_err
-        """
-        return self.x, self.y, self.y_err
+    #     Returns:
+    #         Tuple[Iterable[float], Iterable[float], Iterable[float]]: x,y,y_err
+    #     """
+    #     return self.x, self.y, self.y_err
 
-    def algebraic_area(self):
-        return np.trapz(x=self.x, y=self.y)
+    # def algebraic_area(self):
+    #     return np.trapz(x=self.x, y=self.y)
 
-    def algebraic_area_err(self):
-        return np.sqrt(np.trapz(x=self.x, y=self.y_err**2))
+    # def algebraic_area_err(self):
+    #     return np.sqrt(np.trapz(x=self.x, y=self.y_err**2))
 
-    def get_cpy_on_interval(self, energy_interval: EnergyInterval):
-        """Returns the portion of the spectrum on the given interval
+    # def get_cpy_on_interval(self, energy_interval: EnergyInterval):
+    #     """Returns the portion of the spectrum on the given interval
 
-        Args:
-            left (float): left value of the interval
-            right (float): right value of the interval
+    #     Args:
+    #         left (float): left value of the interval
+    #         right (float): right value of the interval
 
-        Returns:
-            SpectrumBase: portion of the spectrum on [left, right]
-        """
-        cpy = self._get_cpy_on_interval(
-            energy_interval.left, energy_interval.right, None)
+    #     Returns:
+    #         SpectrumBase: portion of the spectrum on [left, right]
+    #     """
+    #     cpy = self._get_cpy_on_interval(
+    #         energy_interval.left, energy_interval.right, None)
 
-        return type(self)(x=cpy.x, y=cpy.y, y_err=cpy.y_err)
+    #     return type(self)(x=cpy.x, y=cpy.y, y_err=cpy.y_err)
 
-    def _get_cpy_on_interval(self, left: float, right: float = None, hole: bool = None):
-        """
-        This function returns the spectrum on the given interval.
+    # def _get_cpy_on_interval(self, left: float, right: float = None, hole: bool = None):
+    #     """
+    #     This function returns the spectrum on the given interval.
 
-        If the right parameter is not passed and hole=True, then the right
-        energy bound will be so that the height at that point
-        is not greater than the height at the first point, else
-        the resulting spectrum will contain all the right points
-        starting at the left point.
-        """
+    #     If the right parameter is not passed and hole=True, then the right
+    #     energy bound will be so that the height at that point
+    #     is not greater than the height at the first point, else
+    #     the resulting spectrum will contain all the right points
+    #     starting at the left point.
+    #     """
 
-        x, y, y_err = self.x, self.y, self.y_err
+    #     x, y, y_err = self.x, self.y, self.y_err
 
-        new_x = []
-        new_y = []
-        new_y_err = []
-        zipped = zip(x, y, y_err)
-        if right != None:
-            for x_i, y_i, y_err in zipped:
-                if(left <= x_i <= right):
-                    new_x += [x_i]
-                    new_y += [y_i]
-                    new_y_err += [y_err]
-        elif hole:
-            max_height = 0
-            for x_i, y_i, y_err in zipped:
-                if left <= x_i:
-                    if(max_height > 0 and y_i >= max_height):
-                        break
-                    new_x += [x_i]
-                    new_y += [y_i]
-                    new_y_err += [y_err]
-                    if(max_height <= 0):
-                        max_height = new_y[0]
+    #     new_x = []
+    #     new_y = []
+    #     new_y_err = []
+    #     zipped = zip(x, y, y_err)
+    #     if right != None:
+    #         for x_i, y_i, y_err in zipped:
+    #             if(left <= x_i <= right):
+    #                 new_x += [x_i]
+    #                 new_y += [y_i]
+    #                 new_y_err += [y_err]
+    #     elif hole:
+    #         max_height = 0
+    #         for x_i, y_i, y_err in zipped:
+    #             if left <= x_i:
+    #                 if(max_height > 0 and y_i >= max_height):
+    #                     break
+    #                 new_x += [x_i]
+    #                 new_y += [y_i]
+    #                 new_y_err += [y_err]
+    #                 if(max_height <= 0):
+    #                     max_height = new_y[0]
 
-        elif hole == False:
-            max_height = 0
-            for x_i, y_i, y_err in zipped:
-                if left <= x_i:
-                    if(max_height > 0 and y_i <= max_height):
-                        break
-                    new_x += [x_i]
-                    new_y += [y_i]
-                    new_y_err += [y_err]
+    #     elif hole == False:
+    #         max_height = 0
+    #         for x_i, y_i, y_err in zipped:
+    #             if left <= x_i:
+    #                 if(max_height > 0 and y_i <= max_height):
+    #                     break
+    #                 new_x += [x_i]
+    #                 new_y += [y_i]
+    #                 new_y_err += [y_err]
 
-                    if(max_height <= 0):
-                        max_height = new_y[0]
+    #                 if(max_height <= 0):
+    #                     max_height = new_y[0]
 
-        return SpectrumBase(np.array(new_x), np.array(new_y), np.array(new_y_err))
+    #     return SpectrumBase(np.array(new_x), np.array(new_y), np.array(new_y_err))
 
-    def average_y(self, energy_interval: EnergyInterval) -> float:
-        subspectrum = self.get_cpy_on_interval(energy_interval)
-        return np.sum(subspectrum.y)/len(subspectrum.y)
+    # def average_y(self, energy_interval: EnergyInterval) -> float:
+    #     subspectrum = self.get_cpy_on_interval(energy_interval)
+    #     return np.sum(subspectrum.y)/len(subspectrum.y)
 
-    def average(self, energy_interval: EnergyInterval = None) -> Tuple[ValueAndError, float]:
+    # def average(self, energy_interval: EnergyInterval = None) -> Tuple[ValueAndError, float]:
 
-        if energy_interval == None:
-            energy_interval = EnergyInterval(self.x[0], self.x[-1])
+    #     if energy_interval == None:
+    #         energy_interval = EnergyInterval(self.x[0], self.x[-1])
 
-        subspectrum = self.get_cpy_on_interval(energy_interval)
+    #     subspectrum = self.get_cpy_on_interval(energy_interval)
 
-        aver_x = np.sum(subspectrum.x)/len(subspectrum.x)
-        index = get_interval_index_log(aver_x, Interval2D(
-            energy_interval.left, energy_interval.right), len(subspectrum.x))
-        aver_y = np.sum(subspectrum.y)/len(subspectrum.y)
-        aver_y_err = np.sqrt(np.sum(subspectrum.y_err**2)) / \
-            len(subspectrum.y_err)
+    #     aver_x = np.sum(subspectrum.x)/len(subspectrum.x)
+    #     index = get_interval_index_log(aver_x, Interval2D(
+    #         energy_interval.left, energy_interval.right), len(subspectrum.x))
+    #     aver_y = np.sum(subspectrum.y)/len(subspectrum.y)
+    #     aver_y_err = np.sqrt(np.sum(subspectrum.y_err**2)) / \
+    #         len(subspectrum.y_err)
 
-        return ValueAndError(value=aver_y, err=aver_y_err), subspectrum.x[index]
+    #     return ValueAndError(value=aver_y, err=aver_y_err), subspectrum.x[index]
 
-    def std_y(self, energy_interval: EnergyInterval) -> float:
-        return np.std(self.get_cpy_on_interval(energy_interval).y)
+    # def std_y(self, energy_interval: EnergyInterval) -> float:
+    #     return np.std(self.get_cpy_on_interval(energy_interval).y)
 
-    def x_interval(self):
-        return self.x[0], self.x[-1]
-
+    # def x_interval(self):
+    #     return self.x[0], self.x[-1]
 
 
 class SpectrumCount(SpectrumBase):
@@ -214,7 +218,7 @@ class PoissonSpectrumCountFactory:
     will be the square root of the number of
     particles.
     """
-    
+
     @staticmethod
     def build_spectrum_count(*files: Iterable[str]) -> SpectrumCount:
         """This factory method returns the particle distribution
@@ -234,5 +238,21 @@ class PoissonSpectrumCountFactory:
         y_error = np.sqrt(y)
 
         return SpectrumCount(x, y, y_error)
-    
 
+    @staticmethod
+    def build_log_empty_spectrumcount(hv_left: float, hv_right: float, n_intervals: int):
+
+        d_nh_expo = (np.log10(hv_right)-np.log10(hv_left))/n_intervals
+        hv_list = []
+
+        for i in range(n_intervals):
+            hv_ip1 = hv_left*10**(d_nh_expo*(i+1))
+            hv_i = hv_left*10**(d_nh_expo*i)
+            d_hv = hv_ip1 - hv_i
+            hv_list += [hv_i + (d_hv/2)]
+
+        x = np.array(hv_list)
+        y = np.zeros(len(x))
+        y_err = np.zeros(len(x))
+
+        return SpectrumBase(x, y, y_err)
