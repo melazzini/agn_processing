26bfa7d3b51f6569d2346dfed6a1af527f0b218a
francisco melazzini <francisco.melazzini@physics.msu.ru>
2023-03-29 12:22:08 +0300

    added .gitignore and tested a little bit photon_register_policy module

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from photon_register_policy import PhotonRawInfo, PhotonType, FluorescentLine
+import numpy as np
+
 print('hello world')
+
+photonRawInfo = PhotonRawInfo(
+    hv=6404.7,
+    theta=0.1,
+    phi=0.1,
+    photonType=PhotonType('1'),
+    line=FluorescentLine('13'),
+    numOfScatterings=1,
+    totalPathLength=1e12,
+    numOfClouds=3,
+    escapePosition=np.array([1,2,3]),
+    effectiveLength=1e8
+    )
