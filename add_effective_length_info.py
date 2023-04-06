535aeebbc2a5cddff6669ca04cbc302432b68441
francisco melazzini <francisco.melazzini@physics.msu.ru>
2023-04-06 11:07:53 +0300

    improved add_effective_lengths.py

--- add_effective_length_info.py
+++ add_effective_length_info.py
@@ -9,12 +9,13 @@ update_effective_length_6_columns = update_effective_length_6_columns
 
 print('=====================================')
 for sim_dir in listdir(root_dir):
-    if sim_dir == 'past' or '-1' in sim_dir:
-        print(f'past or smooth torus: {sim_dir}')
+    sim_root_dir = path.join(root_dir, sim_dir)
+    if sim_dir == 'past' or path.isfile(sim_root_dir) or 'data' not in listdir(sim_root_dir):
+        print(f'Unknown path:: {sim_root_dir}')
         continue
     print(f'processing sim dir: {sim_dir}')
     sim_info = AgnSimulationInfo.build_agn_simulation_info(
-        sim_root_dir=path.join(root_dir, sim_dir))
+        sim_root_dir=sim_root_dir)
     print(sim_info)
 
     photon_files = reduce(lambda file_i, file_j: file_i +
@@ -23,6 +24,9 @@ for sim_dir in listdir(root_dir):
     rv = subprocess.call([update_effective_length_6_columns,
                           str(sim_info.r_clouds/100),
                           str(sim_info.n_clouds),
+                          str(sim_info.r1),
+                          str(sim_info.r2),
+                          str(sim_info.theta),
                           sim_info.clouds_file_path,
                           *sim_info.simulation_files])
 
